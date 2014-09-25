4111f481707e4dcd98684fdf79d14fbff46619cc
Jad Bitar <jadbitar@mac.com>
2014-09-25 02:28:10 -0400

    Pass options to `MenuFactory::createItem()`

--- src/MenuTrait.php
+++ src/MenuTrait.php
@@ -7,13 +7,13 @@ trait MenuTrait {
 	protected $_factory;
 	protected $_menus = [];
 
-	public function get($name) {
+	public function get($name, array $options = []) {
 		if (empty($this->_factory)) {
 			$this->_factory = new MenuFactory();
 		}
 
 		if (empty($this->_menus[$name])) {
-			$this->_menus[$name] = $this->_factory->createItem($name);
+			$this->_menus[$name] = $this->_factory->createItem($name, $options);
 		}
 
 		return $this->_menus[$name];
